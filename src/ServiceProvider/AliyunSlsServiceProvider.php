15e807a773683134ab641459905e18904c96cd69
T-99w <100969997+T-99w@users.noreply.github.com>
2022-08-15 19:33:51 +0800

    Add files via upload

---
+++ src/ServiceProvider/AliyunSlsServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace T99w\Aliyunlogsls\ServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class AliyunSlsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/aliyunlog.php' => config_path('aliyunlog.php'),
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/aliyunlog.php', 'aliyunlog'
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
